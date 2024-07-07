673c1a6a0d638e6813430755d7dd4a0cdb48c797
vitor-bitencourt <vitorbitencourt8486@gmail.com>
2024-07-07 16:10:49 -0300

    feat: creating the model for the db

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
+//Model
+const Order = mongoose.model('Order', { 
+  orderId: { type: String, unique: true },
+  value: { type: Number, required: true },
+  creationDate: { type: Date, required: true },
+  items: [{
+    productId: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true }
+  }]
+});
+
 app.listen(port, () => {
   mongoose.connect(process.env.MONGODB_URI, {
   }).then(() => {
